2d9e470d1950ea94685cac01e650a01a30f6e6db
dwane richards <dwanerich@gmail.com>
2020-01-28 02:20:25 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,14 @@
+
+
 require 'pry'
 
 class Artist
+
   attr_accessor :name
   attr_reader :songs
+
+  extend Memorable::ClassMethods
+  # include Memorable::InstanceMethods
 
   @@artists = []
 
--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,14 @@
+
+
 require 'pry'
 
 class Song
+
   attr_accessor :name
   attr_reader :artist
+
+  extend Memorable::ClassMethods
+  # include Memorable::InstanceMethods
 
   @@songs = []
 
